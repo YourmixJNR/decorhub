7feabf9041e4706c5b9407c62f3e6babcc22855e
YourmixJNR <radmakson@gmail.com>
2023-12-27 11:47:32 +0100

    Nav config

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as MenuIcon } from "../../assets/icons/menu-line-horizontal.svg";
+import { ReactComponent as CloseMenu } from "../../assets/icons/Line.svg";
 import { ReactComponent as CartIcon } from "../../assets/icons/shopping-bag.svg";
 import { ReactComponent as SearchTwo } from "../../assets/icons/search-02.svg";
 import { ReactComponent as UserCircle } from "../../assets/icons/user-circle.svg";
@@ -14,11 +15,16 @@ const Header = () => {
     { name: "Contact Us", path: "#" },
   ];
 
+  const [open, setOpen] = useState(false);
+
   return (
     <header className="flex items-center justify-between bg-white px-8 py-4">
       {/* Left Section */}
       <div className="flex items-center">
-        <MenuIcon className="md:hidden" /> <Logo />
+        <div className="md:hidden" onClick={()=>setOpen(!open)}>
+          {open ? <MenuIcon /> : <CloseMenu />}
+        </div>{" "}
+        <Logo />
       </div>
 
       {/* Menu Section */}
@@ -35,13 +41,15 @@ const Header = () => {
       </div>
 
       {/* Right Section */}
-      <div className="flex items-center">
+      <div className="flex items-center space-x-5">
         <SearchTwo className="hidden md:block" />
         <UserCircle className="hidden md:block" />
-        <CartIcon />
-        <span className="text-base bg-black text-white px-1.5 py-0.3 rounded-full">
-          2
-        </span>
+        <div className="flex items-center space-x-2">
+          <CartIcon />
+          <span className="text-base bg-black text-white px-1.5 py-0.2 rounded-full">
+            2
+          </span>
+        </div>
       </div>
     </header>
   );
