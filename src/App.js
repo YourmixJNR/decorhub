a486fdd12fe787239d7df95dba53d40abe7d045e
YourmixJNR <radmakson@gmail.com>
2023-12-27 03:32:47 +0100

    tailwindcss setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/layout/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <p className='bg-black text-white'>Hello World</p>
+    </div>
+  )
+}
+
+export default Header
