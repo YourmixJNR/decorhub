6680d06d30bdccca52c2e5a9077034da5a9da9aa
YourmixJNR <radmakson@gmail.com>
2023-12-28 01:07:20 +0100

    CTA

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -13,105 +13,112 @@ import Logo from "../logo/Logo";
 import SearchBar from "../ui/SearchBar";
 import MobileNavBar from "../navbar/MobileNavBar";
 import DesktopNavBar from "../navbar/DesktopNavBar";
+import TopCTA from "../ui/TopCTA";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <header className="flex items-center justify-between bg-white px-8 py-4">
-      {/* Desktop Starts */}
-      <div className="flex items-center">
-        <div className="md:hidden">
-          <MenuIcon className="cursor-pointer" onClick={() => setOpen(!open)} />
-        </div>{" "}
-        <Logo />
-      </div>
+    <>
+      <TopCTA />
+      <header className="flex items-center justify-between bg-white px-8 py-4">
+        {/* Desktop Starts */}
+        <div className="flex items-center">
+          <div className="md:hidden">
+            <MenuIcon
+              className="cursor-pointer"
+              onClick={() => setOpen(!open)}
+            />
+          </div>{" "}
+          <Logo />
+        </div>
 
-      <div className="space-x-10 hidden md:block">
-        <DesktopNavBar />
-      </div>
+        <div className="space-x-10 hidden md:block">
+          <DesktopNavBar />
+        </div>
 
-      <div className="flex items-center space-x-5">
-        <Link to="#">
-          <SearchTwo className="hidden md:block" />
-        </Link>
-        <Link to="#">
-          <UserCircle className="hidden md:block" />
-        </Link>
-        <Link to="#">
-          <div className="flex items-center space-x-2">
-            <CartIcon />
-            <span className="text-base bg-black text-white px-2 py-0.5 rounded-full">
-              2
-            </span>
-          </div>
-        </Link>
-      </div>
-      {/* Desktop Ends */}
-
-      {/* Mobile Starts */}
-      <div
-        className={`w-full h-full top-0 left-0 bg-gray-400 absolute transform origin-left transition-transform duration-300 ease-in-out ${
-          open ? "translate-x-0" : "-translate-x-full"
-        } md:hidden`}
-      >
-        <div className="w-11/12 h-full bg-white px-6 py-6 flex flex-col justify-between">
-          <div>
-            <div className="flex justify-between mb-4">
-              <Logo />
-              <CloseMenu
-                className="cursor-pointer"
-                onClick={() => setOpen(!open)}
-              />
+        <div className="flex items-center space-x-5">
+          <Link to="#">
+            <SearchTwo className="hidden md:block" />
+          </Link>
+          <Link to="#">
+            <UserCircle className="hidden md:block" />
+          </Link>
+          <Link to="#">
+            <div className="flex items-center space-x-2">
+              <CartIcon />
+              <span className="text-base bg-black text-white px-2 py-0.5 rounded-full">
+                2
+              </span>
             </div>
-            <SearchBar />
-            <MobileNavBar />
-          </div>
+          </Link>
+        </div>
+        {/* Desktop Ends */}
 
-          <div>
-            <div className="flex justify-between border-b border-gray-200 pb-2 mb-4 ">
-              <span className="text-gray-500">Cart</span>
-              <div className="flex items-center space-x-2">
-                <CartIcon />
-                <span className="text-base bg-black text-white px-2 py-0.5 rounded-full">
-                  2
-                </span>
+        {/* Mobile Starts */}
+        <div
+          className={`w-full h-full top-0 left-0 bg-gray-400 absolute transform origin-left transition-transform duration-300 ease-in-out ${
+            open ? "translate-x-0" : "-translate-x-full"
+          } md:hidden`}
+        >
+          <div className="w-11/12 h-full bg-white px-6 py-6 flex flex-col justify-between">
+            <div>
+              <div className="flex justify-between mb-4">
+                <Logo />
+                <CloseMenu
+                  className="cursor-pointer"
+                  onClick={() => setOpen(!open)}
+                />
               </div>
+              <SearchBar />
+              <MobileNavBar />
             </div>
 
-            <div className="flex justify-between border-b pb-2 mb-4 border-gray-200">
-              <span className="text-gray-500">Wishlist</span>
-              <div className="flex items-center space-x-2">
-                <WishList />
-                <span className="text-base bg-black text-white px-2 py-0.5 rounded-full">
-                  2
-                </span>
+            <div>
+              <div className="flex justify-between border-b border-gray-200 pb-2 mb-4 ">
+                <span className="text-gray-500">Cart</span>
+                <div className="flex items-center space-x-2">
+                  <CartIcon />
+                  <span className="text-base bg-black text-white px-2 py-0.5 rounded-full">
+                    2
+                  </span>
+                </div>
               </div>
-            </div>
 
-            <button
-              type="submit"
-              className="bg-black w-full flex justify-center text-white rounded-md p-2 mb-5"
-            >
-              Sign In
-            </button>
+              <div className="flex justify-between border-b pb-2 mb-4 border-gray-200">
+                <span className="text-gray-500">Wishlist</span>
+                <div className="flex items-center space-x-2">
+                  <WishList />
+                  <span className="text-base bg-black text-white px-2 py-0.5 rounded-full">
+                    2
+                  </span>
+                </div>
+              </div>
 
-            <div className="flex space-x-6">
-              <Link to="#">
-                <Instagram />
-              </Link>
-              <Link to="#">
-                <Facebook />
-              </Link>
-              <Link to="#">
-                <Youtube />
-              </Link>
+              <button
+                type="submit"
+                className="bg-black w-full flex justify-center text-white rounded-md p-2 mb-5"
+              >
+                Sign In
+              </button>
+
+              <div className="flex space-x-6">
+                <Link to="#">
+                  <Instagram />
+                </Link>
+                <Link to="#">
+                  <Facebook />
+                </Link>
+                <Link to="#">
+                  <Youtube />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {/* Mobile Ends */}
-    </header>
+        {/* Mobile Ends */}
+      </header>
+    </>
   );
 };
 
---
+++ src/components/ui/TopCTA.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as RightIcon } from "../../assets/icons/arrow-right.svg";
+import { ReactComponent as TicketPercent } from "../../assets/icons/ticket-percent.svg";
+import { ReactComponent as CloseIcon } from "../../assets/icons/Line.svg";
+
+const TopCTA = () => {
+  const [close, setClose] = useState(false);
+
+  return (
+    <div
+      className={`bg-slate-100 justify-center items-center space-x-5 px-8 py-2 ${
+        close ? "hidden" : "flex"
+      }`}
+    >
+      <div className="flex justify-center items-center space-x-2">
+        <TicketPercent />
+        <span className="text-sm">30% off storewide — Limited time!</span>
+        <Link className="hidden space-x-2 md:flex text-sm items-center justify-center underline text-blue-400" to="#">
+           <span>Shop Now</span> <RightIcon />
+        </Link>
+      </div>
+      <CloseIcon className="cursor-pointer" onClick={() => setClose(true)} />
+    </div>
+  );
+};
+
+export default TopCTA;
