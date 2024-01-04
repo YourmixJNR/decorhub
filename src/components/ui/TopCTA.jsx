c1fdb22b8799a9d89f10f98bf9fbab2dd8c77af5
YourmixJNR <radmakson@gmail.com>
2024-01-04 16:45:06 +0100

    init

--- src/components/ui/TopCTA.jsx
+++ src/components/ui/TopCTA.jsx
@@ -8,19 +8,38 @@ const TopCTA = () => {
   const [close, setClose] = useState(false);
 
   return (
+  //   <div
+  //     className={`bg-slate-100 justify-center items-center space-x-5 px-8 py-2 ${
+  //       close ? "hidden" : "grid"
+  //     }`}
+  //   >
+  //     <div className="flex justify-center items-center space-x-2">
+  //       <TicketPercent />
+  //       <span className="text-sm">30% off storewide — Limited time!</span>
+  //       <Link className="hidden space-x-2 md:flex text-sm items-center justify-center underline text-blue-400" to="#">
+  //          <span>Shop Now</span> <RightIcon />
+  //       </Link>
+  //     </div>
+  //     <CloseIcon className="cursor-pointer" onClick={() => setClose(true)} />
+  //   </div>
+  // );
     <div
-      className={`bg-slate-100 justify-center items-center space-x-5 px-8 py-2 ${
-        close ? "hidden" : "flex"
+      className={`bg-slate-100 grid-cols-2 space-x-5 px-8 py-2 ${
+        close ? "hidden" : "grid"
       }`}
     >
-      <div className="flex justify-center items-center space-x-2">
-        <TicketPercent />
-        <span className="text-sm">30% off storewide — Limited time!</span>
-        <Link className="hidden space-x-2 md:flex text-sm items-center justify-center underline text-blue-400" to="#">
-           <span>Shop Now</span> <RightIcon />
-        </Link>
+      <div className="inline-block justify-self-center">
+        <div className="flex justify-center items-center space-x-2">
+          <TicketPercent />
+          <span className="text-sm">30% off storewide — Limited time!</span>
+          <Link className="hidden space-x-2 md:flex text-sm items-center justify-center underline text-blue-400" to="#">
+             <span>Shop Now</span> <RightIcon />
+          </Link>
+        </div>
       </div>
-      <CloseIcon className="cursor-pointer float-end" onClick={() => setClose(true)} />
+      <div className="inline-block justify-self-end">
+        <CloseIcon className="cursor-pointer" onClick={() => setClose(true)} />
+      </div>
     </div>
   );
 };
