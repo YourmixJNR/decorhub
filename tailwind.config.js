7633ebeaa5fc052ee54ef6930cdf822b6475da2e
YourmixJNR <radmakson@gmail.com>
2024-01-05 18:06:16 +0100

    config fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      zIndex: {
-        '100': '100',
-      }
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
     },
   },
   plugins: [],
