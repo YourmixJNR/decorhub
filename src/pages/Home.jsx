c05547987491a127a6f39a08fbb139be00f7495c
YourmixJNR <radmakson@gmail.com>
2024-01-05 14:52:07 +0100

    init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Header from "../components/layout/Header";
 import ImagePlaceholder1 from "../assets/images/ImagePlaceholder.jpg";
 import ImagePlaceholder2 from "../assets/images/Pasteimage.png";
 import ImagePlaceholder3 from "../assets/images/ImagePlaceholder.jpg";
+import { ReactComponent as LeftArrow } from "../assets/icons/arrow-left.svg"
+import { ReactComponent as RightArrow } from "../assets/icons/arrow-right.svg"
 import './Home.css'
 
 const Home = () => {
@@ -24,27 +26,27 @@ const Home = () => {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000); // Change slide every 5000 milliseconds (5 seconds)
+    }, 5000);
 
-    return () => clearInterval(intervalId); // Cleanup the interval on component unmount
+    return () => clearInterval(intervalId);
   }, [currentImageIndex, images.length]);
 
   return (
     <div>
       <Header />
       <div
-        className="bg-custom-image bg-cover bg-center h-80 mx-8 lg:mx-40 relative z-0"
+        className="bg-custom-image bg-cover bg-center h-80 mx-8 lg:mx-40 lg:h-[33rem] relative z-0"
         style={{
           backgroundImage: `url(${images[currentImageIndex]})`,
         }}
       >
-        <div className="hidden md:flex">
-          <button onClick={handlePrevClick} className="absolute left-0 top-1/2">
-            Prev
-          </button>
-          <button onClick={handleNextClick} className="absolute right-0 top-1/2">
-            Next
-          </button>
+        <div className="hidden px-8 md:flex">
+          <span onClick={handlePrevClick} className="bg-white w-12 h-12 p-3 flex justify-center items-center rounded-full absolute left-8 top-1/2">
+            <LeftArrow />
+          </span>
+          <span onClick={handleNextClick} className="bg-white w-12 h-12 p-3 flex justify-center items-center rounded-full absolute right-8 top-1/2">
+            <RightArrow />
+          </span>
         </div>
 
         <div className="absolute flex gap-4 top-[90%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
