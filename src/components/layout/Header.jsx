0875e44721416eaa9fbbddbc2dab7f49f0e33a0a
YourmixJNR <radmakson@gmail.com>
2024-01-10 17:01:58 +0100

    set Header to fixed

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -11,103 +11,105 @@ import HeaderSocialIcons from "../ui/HeaderSocialIcons";
 import SearchBar from "../ui/NavSearchBar";
 import MobileNavBar from "../navbar/MobileNavBar";
 import DesktopNavBar from "../navbar/DesktopNavBar";
-import TopCTA from "../ui/NotificationBar";
+import NotificationBar from "../ui/NotificationBar";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <>
-      <TopCTA />
-      <header className="flex items-center justify-between bg-white mx-8 py-4 xl:mx-40 z-100">
-        {/* Desktop Starts */}
-        <div className="flex items-center">
-          <div className="md:hidden">
-            <MenuIcon
-              className="cursor-pointer"
-              onClick={() => setOpen(!open)}
-            />
+      <header style={{ zIndex: 50 }} className="fixed w-full">
+        <NotificationBar />
+        <div className=" flex items-center justify-between bg-white px-8 py-4 xl:px-40">
+          {/* Desktop Starts */}
+          <div className="flex items-center">
+            <div className="md:hidden">
+              <MenuIcon
+                className="cursor-pointer"
+                onClick={() => setOpen(!open)}
+              />
+            </div>
+            <Logo />
           </div>
-          <Logo />
-        </div>
 
-        <div className="space-x-10 hidden md:block">
-          <DesktopNavBar />
-        </div>
+          <div className="space-x-10 hidden md:block">
+            <DesktopNavBar />
+          </div>
 
-        <div className="flex items-center space-x-5">
-          <Link to="#">
-            <SearchTwo className="hidden md:block" />
-          </Link>
-          <Link to="#">
-            <UserCircle className="hidden md:block" />
-          </Link>
-          <Link to="#">
-            <div className="flex items-center space-x-1">
-              <CartIcon />
-              <span className="text-xs font-inter font-bold bg-black text-white w-5 h-5 justify-center items-center flex rounded-full">
-                2
-              </span>
-            </div>
-          </Link>
-        </div>
-        {/* Desktop Ends */}
-
-        {/* Mobile Starts */}
-        <div
-          className={`w-full h-full top-0 left-0 bg-gray-400 absolute transform origin-left transition-transform duration-300 ease-in-out ${
-            open ? "translate-x-0" : "-translate-x-full"
-          } md:hidden`}
-          style={{ zIndex: 1000 }}
-        >
-          <div className="w-11/12 h-full bg-white px-6 py-6 flex flex-col justify-between">
-            <div>
-              <div className="flex justify-between mb-4">
-                <Logo />
-                <CloseMenu
-                  className="cursor-pointer"
-                  onClick={() => setOpen(!open)}
-                />
+          <div className="flex items-center space-x-5">
+            <Link to="#">
+              <SearchTwo className="hidden md:block" />
+            </Link>
+            <Link to="#">
+              <UserCircle className="hidden md:block" />
+            </Link>
+            <Link to="#">
+              <div className="flex items-center space-x-1">
+                <CartIcon />
+                <span className="text-xs font-inter font-bold bg-black text-white w-5 h-5 justify-center items-center flex rounded-full">
+                  2
+                </span>
               </div>
-              <SearchBar />
-              <MobileNavBar />
-            </div>
+            </Link>
+          </div>
+          {/* Desktop Ends */}
 
-            <div>
-              <div className="flex justify-between border-b border-gray-200 pb-2 mb-4 ">
-                <span className="text-gray-500">Cart</span>
-                <div className="flex items-center space-x-1">
-                  <CartIcon />
-                  <span className="text-xs font-inter font-bold bg-black text-white w-5 h-5 justify-center items-center flex rounded-full">
-                    2
-                  </span>
+          {/* Mobile Starts */}
+          <div
+            className={`w-full h-full top-0 left-0 bg-gray-400 fixed transform origin-left transition-transform duration-300 ease-in-out ${
+              open ? "translate-x-0" : "-translate-x-full"
+            } md:hidden`}
+            style={{ zIndex: 100 }}
+          >
+            <div className="w-11/12 h-full bg-white px-6 py-6 flex flex-col justify-between">
+              <div>
+                <div className="flex justify-between mb-4">
+                  <Logo />
+                  <CloseMenu
+                    className="cursor-pointer"
+                    onClick={() => setOpen(!open)}
+                  />
+                </div>
+                <SearchBar />
+                <MobileNavBar />
+              </div>
+
+              <div>
+                <div className="flex justify-between border-b border-gray-200 pb-2 mb-4 ">
+                  <span className="text-gray-500">Cart</span>
+                  <div className="flex items-center space-x-1">
+                    <CartIcon />
+                    <span className="text-xs font-inter font-bold bg-black text-white w-5 h-5 justify-center items-center flex rounded-full">
+                      2
+                    </span>
+                  </div>
+                </div>
+
+                <div className="flex justify-between border-b pb-2 mb-4 border-gray-200">
+                  <span className="text-gray-500">Wishlist</span>
+                  <div className="flex items-center space-x-1">
+                    <WishList />
+                    <span className="text-xs font-inter font-bold bg-black text-white w-5 h-5 justify-center items-center flex rounded-full">
+                      2
+                    </span>
+                  </div>
+                </div>
+
+                <button
+                  type="submit"
+                  className="font-inter text-xl font-medium bg-black w-full flex justify-center text-white rounded-md p-2 mb-5"
+                >
+                  Sign In
+                </button>
+
+                <div className="flex space-x-6">
+                  <HeaderSocialIcons />
                 </div>
               </div>
-
-              <div className="flex justify-between border-b pb-2 mb-4 border-gray-200">
-                <span className="text-gray-500">Wishlist</span>
-                <div className="flex items-center space-x-1">
-                  <WishList />
-                  <span className="text-xs font-inter font-bold bg-black text-white w-5 h-5 justify-center items-center flex rounded-full">
-                    2
-                  </span>
-                </div>
-              </div>
-
-              <button
-                type="submit"
-                className="font-inter text-xl font-medium bg-black w-full flex justify-center text-white rounded-md p-2 mb-5"
-              >
-                Sign In
-              </button>
-
-              <div className="flex space-x-6">
-                <HeaderSocialIcons />
-              </div>
             </div>
           </div>
+          {/* Mobile Ends */}
         </div>
-        {/* Mobile Ends */}
       </header>
     </>
   );
