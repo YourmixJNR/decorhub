f3d65cdc4df5aa78f6c0dd5ba7dbd047e18cd4d2
YourmixJNR <radmakson@gmail.com>
2024-01-11 14:35:17 +0100

    Added ProductPage

---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+const ProductPage = () => {
+  return (
+    <>
+      <Layout>
+        
+      </Layout>
+    </>
+  )
+}
+
+export default ProductPage
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "../pages/Homepage";
+import ProductPage from "../pages/ProductPage";
 
 const RootRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/productpage" element={<ProductPage />} />
     </Routes>
   );
 };
