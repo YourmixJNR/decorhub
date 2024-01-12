bade4c10ca35c180d0a44e5f1eda97fba1988805
YourmixJNR <radmakson@gmail.com>
2024-01-12 10:00:44 +0100

    Added Scroll to top function

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import Homepage from "../pages/Homepage";
 import ProductPage from "../pages/ProductPage";
+import ScrollToTop from "./ScrollToTop";
 
 const RootRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/productpage" element={<ProductPage />} />
-    </Routes>
+    <ScrollToTop>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/productpage" element={<ProductPage />} />
+      </Routes>
+    </ScrollToTop>
   );
 };
 
